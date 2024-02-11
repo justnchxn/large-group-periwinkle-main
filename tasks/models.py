817f625c05f1d55d7513fd7c7846cd4dc4d53568
Justin Chan <justnchxn@gmail.com>
2024-02-11 23:44:39 +0000

    init

---
+++ tasks/models.py
@@ -0,0 +1,55 @@
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from libgravatar import Gravatar
+
+class User(AbstractUser):
+    """Model used for user authentication, and team member related information."""
+
+    username = models.CharField(
+        max_length=30,
+        unique=True,
+        validators=[RegexValidator(
+            regex=r'^@\w{3,}$',
+            message='Username must consist of @ followed by at least three alphanumericals'
+        )]
+    )
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+
+
+    class Meta:
+        """Model options."""
+
+        ordering = ['last_name', 'first_name']
+
+    def full_name(self):
+        """Return a string containing the user's full name."""
+
+        return f'{self.first_name} {self.last_name}'
+
+    def gravatar(self, size=120):
+        """Return a URL to the user's gravatar."""
+
+        gravatar_object = Gravatar(self.email)
+        gravatar_url = gravatar_object.get_image(size=size, default='mp')
+        return gravatar_url
+
+    def mini_gravatar(self):
+        """Return a URL to a miniature version of the user's gravatar."""
+        
+        return self.gravatar(size=60)
+
+class Post(models.Model):
+    """Posts by users in their microblogs."""
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, blank=False)
+    text = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Model options."""
+
+        ordering = ['-created_at']        
